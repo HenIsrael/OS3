614549140bacdcc008d7c9484a76fb00eae7c42b
henIsrael <hen.israel@campus.technion.ac.il>
2023-06-07 18:00:38 +0300

    add request manager struct

---
+++ webserver-files/request_manager.h
@@ -0,0 +1,35 @@
+#ifndef _REQUEST_MANAGER_H
+#define _REQUEST_MANAGER_H
+
+#include "queue.h"
+#include "request_obj.h"
+
+typedef struct request_manager_t{
+    List runningRequests;
+    List waitingRequestsQueue;
+
+    int maxAcceptedRequests;
+    int threadsNum;//todo: remove
+
+}*RequestManager;
+
+RequestManager requestManagerCreate(int threadsNum, int maxAcceptedRequests);
+int requestManagerGetWaitingQueueSize(RequestManager requestManager);
+int requestManagerHasWaitingRequests(RequestManager requestManager);
+int requestManagerCanAcceptRequests(RequestManager requestManager);
+
+RequestObject requestManagerGetReadyRequest(RequestManager requestManager);//todo: return status instead
+
+void requestManagerAddPendingRequest(RequestManager requestManager, RequestObject requestObject);//todo: return status instead
+void requestManagerAddReadyRequest(RequestManager requestManager, RequestObject requestObject);//todo: return status instead
+void requestManagerRemoveFinishedRequest(RequestManager requestManager, RequestObject requestObject);//todo: return status instead
+void requestManagerRemoveRequestFromWaitingQueue(RequestManager requestManager, RequestObject requestObject);//todo: return status instead
+RequestObject requestManagerRemoveRequestFromWaitingQueueAtIndex(RequestManager requestManager, int index);//todo: return status instead
+
+int requestManagerRemoveOldestRequestFromWaitingQueue(RequestManager requestManager);//todo: return status instead
+
+void requestManagerDelete(RequestManager requestManager);
+
+
+void requestManagerPrint(RequestManager requestManager);
+#endif //_REQUEST_MANAGER_H
